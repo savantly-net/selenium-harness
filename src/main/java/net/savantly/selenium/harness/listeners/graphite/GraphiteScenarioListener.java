ae8e0e7c9d99e56477a5751b82710ea0ca2b5256
Jeremy <jdbranham@hotmail.com>
2016-12-15 16:05:09 -0600

    moved graphite metrics match/replace into the properties file

--- src/main/java/net/savantly/selenium/harness/listeners/graphite/GraphiteScenarioListener.java
+++ src/main/java/net/savantly/selenium/harness/listeners/graphite/GraphiteScenarioListener.java
@@ -6,26 +6,29 @@ import java.net.UnknownHostException;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import net.savantly.graphite.CarbonFactory;
 import net.savantly.graphite.CarbonMetric;
 import net.savantly.graphite.GraphiteClient;
-import net.savantly.graphite.impl.DefaultGraphiteClient;
+import net.savantly.graphite.GraphiteClientFactory;
 import net.savantly.graphite.impl.SimpleCarbonMetric;
 import net.savantly.selenium.harness.modules.scenario.ScenarioListener;
 import net.savantly.selenium.harness.modules.scenario.ScenarioResult;
 
+@ConfigurationProperties("graphite.metric")
 public class GraphiteScenarioListener implements ScenarioListener{
 	
 	@Value("${graphite.host}")
 	private String graphiteHost;
 	@Value("${graphite.metric.prefix}")
 	private String metricPrefix;
+	private String[] match;
+	private String[] replace;
 	GraphiteClient graphiteClient;
 	
 	@PostConstruct
 	public void post() throws UnknownHostException, SocketException{
-		graphiteClient = new DefaultGraphiteClient(CarbonFactory.getDefaultCarbonSender(graphiteHost, 2003, "0.0.0.0"));
+		graphiteClient = GraphiteClientFactory.defaultGraphiteClient(graphiteHost);
 	}
 
 	@Override
@@ -33,7 +36,12 @@ public class GraphiteScenarioListener implements ScenarioListener{
 		if(result.getName() == null) return;
 		
 		long epoch = System.currentTimeMillis()/1000;
-		String metricName = metricPrefix + "." + result.getName().toLowerCase().replaceAll("[^a-zA-Z\\d\\s\\.:]", "").replace(' ', '_');
+		String metricName = metricPrefix + "." + result.getName().toLowerCase();
+		for (int i = 0; i < match.length; i++) {
+			String r = replace != null ? replace[i] : "";
+			metricName = metricName.replaceAll(match[i], r);
+		}
+		metricName = metricName.replace(' ', '_');
 		metricName = ensureNoDotsOnEnds(metricName);
 		String value = result.isFailed() ? "0":"1";
         CarbonMetric carbonMetric = new SimpleCarbonMetric(metricName, value, epoch);
@@ -52,6 +60,22 @@ public class GraphiteScenarioListener implements ScenarioListener{
 
 	public void setGraphiteHost(String graphiteHost) {
 		this.graphiteHost = graphiteHost;
+	}
+
+	public String[] getMatch() {
+		return match;
+	}
+
+	public void setMatch(String[] match) {
+		this.match = match;
+	}
+
+	public String[] getReplace() {
+		return replace;
+	}
+
+	public void setReplace(String[] replace) {
+		this.replace = replace;
 	}
 
 }
